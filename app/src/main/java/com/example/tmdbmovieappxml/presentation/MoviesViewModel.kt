ec21ba308124da790e1f693831bed1d900083bc1
ToskePG <danilo.tosic.pmf@gmail.com>
2023-07-19 20:29:54 +0200

    Creates ViewModel and ViewModelProvider. Creates repository for later use.

---
+++ app/src/main/java/com/example/tmdbmovieappxml/presentation/MoviesViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.tmdbmovieappxml.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.tmdbmovieappxml.repository.MovieRepository
+
+class MoviesViewModel(private val moviesRepository: MovieRepository) : ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/tmdbmovieappxml/repository/MovieRepository.kt
@@ -0,0 +1,4 @@
+package com.example.tmdbmovieappxml.repository
+
+class MovieRepository {
+}
