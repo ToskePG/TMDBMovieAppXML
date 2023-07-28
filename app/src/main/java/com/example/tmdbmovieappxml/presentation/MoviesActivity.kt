a532d6bbda8058cabc204a7ef7e10cc948885d22
ToskePG <danilo.tosic.pmf@gmail.com>
2023-07-28 22:25:08 +0200

    Sets up navigation

--- app/src/main/java/com/example/tmdbmovieappxml/presentation/MoviesActivity.kt
+++ app/src/main/java/com/example/tmdbmovieappxml/presentation/MoviesActivity.kt
@@ -2,21 +2,41 @@ package com.example.tmdbmovieappxml.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isInvisible
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.tmdbmovieappxml.R
 import com.example.tmdbmovieappxml.database.MoviesDatabase
 import com.example.tmdbmovieappxml.databinding.ActivityMoviesBinding
 import com.example.tmdbmovieappxml.repository.MovieRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MoviesActivity : AppCompatActivity() {
 
     lateinit var viewModel: MoviesViewModel
     private lateinit var binding: ActivityMoviesBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMoviesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavigation()
         val moviesRepository = MovieRepository(MoviesDatabase(this))
         val viewModelProviderFactory = MoviesViewModelProviderFactory(moviesRepository = moviesRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory)[MoviesViewModel::class.java]
+    }
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentShows) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView = binding.bottomNavBar
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavBar.isInvisible = destination.id == R.id.moviesFragment
+
+        }
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
