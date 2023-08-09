97c70e83433cb19de819042fcad85db59176b0a2
ToskePG <danilo.tosic.pmf@gmail.com>
2023-08-09 13:11:30 +0200

    Refactors

--- app/src/main/java/com/example/tmdbmovieappxml/presentation/MoviesViewModelProviderFactory.kt
+++ app/src/main/java/com/example/tmdbmovieappxml/presentation/MoviesViewModelProviderFactory.kt
@@ -4,7 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.tmdbmovieappxml.repository.MovieRepository
 
-class MoviesViewModelProviderFactory(val moviesRepository: MovieRepository
+class MoviesViewModelProviderFactory(
+    val moviesRepository: MovieRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MoviesViewModel(moviesRepository) as T
